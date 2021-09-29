5756e279e24b93869c0acc624a360ed07f93c09c
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-09-29 09:35:07 +0530

    Add files via upload

---
+++ students_marks.py
@@ -0,0 +1,9 @@
+num=int(input("enter any number="))
+i=1
+dict={}
+while i<=num:
+	student=input("enter any student=")
+	marks=int(input("enter any marks="))
+	dict[student]=marks
+	i+=1
+print(dict)
